f66bfa3970f902e22adff4267b5e0a1dc26d904b
tobychuks <samueltobechukwu01@gmail.com>
2025-08-19 06:26:46 -0700

    Updated author info

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//pascalcase
+var size;
+(function (size) {
+    size[size["Small"] = 1] = "Small";
+    size[size["Medium"] = 2] = "Medium";
+    size[size["Large"] = 3] = "Large";
+})(size || (size = {}));
+;
+let mySize = size.Medium;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+//pascalcase
+const enum size {Small= 1, Medium, Large};
+let mySize: size = size.Medium;
+console.log(mySize);
